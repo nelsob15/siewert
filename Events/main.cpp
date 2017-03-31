94504973a84442bae592152ddbf1b91a14dbe42a
nelsob15 <nelsob15@my.erau.edu>
2017-03-31 09:57:29 -0700

    Fourth Commit

---
+++ Events/main.cpp
@@ -0,0 +1,21 @@
+/*
+  Calendar Application - Events test
+
+  Creates an events object for testing event features
+*/
+
+#include <iostream>
+#include "Event.h"
+#include <map>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    Event main; //creates a event object for navigating through events
+
+    main.main_event_menu(); //runs events menu
+
+    return 0;
+}
